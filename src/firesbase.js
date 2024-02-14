a367acd4608ec9f3481e17682cc3be4185297ebd
unknown <qpscm662@naver.com>
2024-02-14 18:50:54 +0900

    env설정

--- src/firesbase.js
+++ src/firesbase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBQ_iDyrFLv3t5ZvjEF2o3rWN_6hZ1zQVo',
-  authDomain: 'nbc-logindata.firebaseapp.com',
-  projectId: 'nbc-logindata',
-  storageBucket: 'nbc-logindata.appspot.com',
-  messagingSenderId: '279576748767',
-  appId: '1:279576748767:web:dab0f34ad9cd55b1fdff6a'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
